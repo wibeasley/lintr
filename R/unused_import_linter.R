0163d3a38e74cb77cc3dbe06532dfdd723560ede
Alexander Rosenstock <alexander.rosenstock@web.de>
2020-12-06 21:06:30 +0100

    remove duplicate functions, implement import searching for packages

--- R/unused_import_linter.R
+++ R/unused_import_linter.R
@@ -6,52 +6,73 @@ get_import_exprs <- function(x) {
   xml2::xml_find_all(x, "//expr[expr[SYMBOL_FUNCTION_CALL[text() = 'library' or text() = 'require']]]")
 }
 
-strip_names <- function(x) {
-  x <- re_substitutes(x, rex(start, some_of(".", quote, "`", "%", "$", "@")), "")
-  x <- re_substitutes(x, rex(some_of(quote, "`", "<", "-", "%", "$", "@"), end), "")
-  x
+get_package_imports <- function(filename) {
+  pkg <- find_package(path = filename)
+  if (is.null(pkg) || !file.exists(file.path(pkg, "NAMESPACE"))) return(character())
+
+  ns <- readLines(file.path(pkg, "NAMESPACE"))
+
+  # Extract package names from import(pkg) and importFrom(pkg,fun)
+  # From https://cran.r-project.org/doc/manuals/r-devel/R-exts.html#Creating-R-packages, Section 1.1.1:
+  # The mandatory ‘Package’ field gives the name of the package.
+  # This should contain only (ASCII) letters, numbers and dot, have at least two characters and
+  # start with a letter and not end in a dot.
+  pkgname_regex <- rex::rex(
+    letter, any_of(alnum, "."), alnum
+  )
+  imports <- rex::re_matches(
+    ns,
+    rex::rex(
+      start, "import", maybe("From"), "(",
+      capture(name = "package", pkgname_regex)
+    )
+  )[, "package"]
+
+  unique(imports[!is.na(imports)])
 }
 
 #' @describeIn linters checks that libraries imported are actually used.
 #' @export
-unused_import_linter <-  function(source_file) {
-  x <- global_xml_parsed_content(source_file)
-  if (is.null(x)) {
-    return()
-  }
-  import_exprs <- get_import_exprs(x)
-  if (length(import_exprs) == 0) {
-    return()
-  }
-  pkg_exprs <- xml2::xml_find_first(import_exprs,"./expr[STR_CONST|SYMBOL]")
-  import_pkgs <- strip_names(as.character(xml2::xml_find_first(pkg_exprs,"./*/node()")))
+unused_import_linter <- function(except_packages = "tidyverse") {
+  function(source_file) {
+    x <- global_xml_parsed_content(source_file)
+    if (is.null(x)) {
+      return()
+    }
+    import_exprs <- get_import_exprs(x)
+    if (length(import_exprs) == 0) {
+      return()
+    }
+    pkg_exprs <- xml2::xml_find_first(import_exprs, "./expr[STR_CONST|SYMBOL]")
+    import_pkgs <- strip_names(as.character(xml2::xml_find_first(pkg_exprs, "./*/node()")))
 
-  function_calls <- get_function_calls(x)
+    function_calls <- get_function_calls(x)
 
-  lapply(seq_along(import_pkgs),
-    function(i) {
-      pkg <- import_pkgs[[i]]
+    lapply(
+      seq_along(import_pkgs),
+      function(i) {
+        pkg <- import_pkgs[[i]]
 
-      # Tidyverse is only used to load other packages
-      if (pkg == "tidyverse") {
-        return()
-      }
+        if (pkg %in% except_packages) {
+          return()
+        }
 
-      package_exports <- getNamespaceExports(pkg)
-      if (!any(package_exports %in% function_calls)) {
-        import_expr <- xml2::as_list(import_exprs[[i]])
-        pkg_expr <- xml2::as_list(pkg_exprs[[i]])
-        line_num <- import_expr@line1
-        Lint(
-          filename = source_file[["filename"]],
-          line_number = line_num,
-          column_number = pkg_expr@col1,
-          type = "warning",
-          message = "import is never used",
-          line = source_file[["file_lines"]][[as.numeric(line_num)]],
-          ranges = list(as.numeric(c(import_expr@col1, import_expr@col2))),
-          linter = "unused_import_linter"
+        package_exports <- getNamespaceExports(pkg)
+        if (!any(package_exports %in% function_calls)) {
+          import_expr <- xml2::as_list(import_exprs[[i]])
+          pkg_expr <- xml2::as_list(pkg_exprs[[i]])
+          line_num <- import_expr@line1
+          Lint(
+            filename = source_file[["filename"]],
+            line_number = line_num,
+            column_number = pkg_expr@col1,
+            type = "warning",
+            message = "import is never used",
+            line = source_file[["file_lines"]][[as.numeric(line_num)]],
+            ranges = list(as.numeric(c(import_expr@col1, import_expr@col2))),
+            linter = "unused_import_linter"
           )
-      }
-    })
+        }
+      })
+  }
 }
